aa685eb07e868e7e2a0912436923b8bd72d63f21
usuario <usuario>
2017-03-21 12:32:05 +0100

    version inicial

---
+++ Practica1/src/main/java/mgcss/practica1/App.java
@@ -0,0 +1,13 @@
+package mgcss.practica1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola Mundo!" );
+    }
+}
